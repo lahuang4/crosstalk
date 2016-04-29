ad83f1f568941aab0dbe2fe3b08df54f5d894fd6
David Wong <dhyhwong@gmail.com>
2016-04-28 21:37:13 -0400

    finished merge, seems to be passing simple tests

--- server/models/tree.js
+++ server/models/tree.js
@@ -6,7 +6,7 @@ var Tree = function() {
 
   tree.root = new Node(); // initialize a dummy root node
   tree.leaves = new Set();
-  tree.leaves.add(tree.root);
+  tree.leaves.add(tree.root._id);
   tree.directory = {};
   tree.directory[tree.root._id] = tree.root;
   tree.versions = {};
@@ -37,17 +37,56 @@ var Tree = function() {
       }
     }
 
-    // start at the peer tree's leaves and traverse up until you find 
+    var isMissing = function(sender, version) {
+      return sender in missingNodes && version >= missingNodes[sender];
+    }
 
+    // start at the peer tree's leaves and traverse up until you find a node that exists in my tree
+    var nodesToProcess = [];
+
+    peerTree.leaves.forEach(function(leafID) {
+      if (!(leafID in tree.directory)) {
+        var copiedNode = peerTree.directory[leafID].clone();
+        tree.directory[leafID] = copiedNode;
+        nodesToProcess.push(leafID);
+      }
+      tree.leaves.add(leafID);
+    });
+
+    for (var i=0; i<nodesToProcess.length; i++) {
+      var nodeID = nodesToProcess[i];
+      peerTree.directory[nodeID].parents.forEach(function(parentID) {
+        if (!(parentID in tree.directory)) {
+          var copiedNode = peerTree.directory[parentID].clone();
+          tree.directory[parentID] = copiedNode;
+          nodesToProcess.push(parentID)
+        }
+        tree.directory[nodeID].addParent(tree.directory[parentID]);
+        tree.leaves.delete(parentID);
+      });
+    }
+
+    // update the version vector
+    for (var server in peerTree.versions) {
+      tree.versions[server] = Math.max(tree.versions[server], peerTree.versions[server]);
+    }
   }
 
-  // TODO: finish implementation
   tree.equals = function(otherTree) {
     if (!tree.root.equals(otherTree.root)) {
       return false;
     }
 
     return _.isEqual(tree.versions, otherTree.versions);
+  }
+
+  tree.clone = function() {
+    var newTree = new Tree();
+    newTree.directory[tree.root._id] = newTree.root;
+    delete newTree.directory[newTree.root._id];
+    newTree.root._id = tree.root._id;
+
+    return newTree;
   }
 
   return tree;
