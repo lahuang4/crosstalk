301e31de21ca2f0b248ae6700bed800807711810
David Wong <dhyhwong@gmail.com>
2016-05-03 17:32:26 -0400

    now using promises to chain async channel calls

--- client/channels.js
+++ client/channels.js
@@ -6,92 +6,107 @@ var client = require('./client.js');
 
 // Requests the directory to create a new channel, and updates the membership list for that channel upon success.
 exports.createChannel = function(channel) {
-  console.log("Creating channel " + channel + ".");
-  request.post(
-    client.directory + "/createChannel",
-    {
-      json: {
-        user: client.username,
-        channel: channel,
-        address: client.address
-      }
-    },
-    function(err, res, body) {
-      if (!err && res.statusCode == 200) {
-        if (body.success) {
-          // Get the members in the channel.
-          client.channels[channel] = body.members;
-          console.log("Successfully created channel " + channel + ", which has members " + body.members + ".");
-        } else if (body.channel_exists) {
-          // If the channel exists already, we're set.
-          console.log("Channel " + channel + " already exists.")
-        } else {
-          console.log("An unhandled error occurred.")
+  console.log("Trying to create channel " + channel + ".");
+  var promise = new Promise(function(resolve, reject) {
+    request.post(
+      client.directory + "/createChannel",
+      {
+        json: {
+          user: client.username,
+          channel: channel,
+          address: client.address
         }
-      } else {
-        exports.createChannel(channel);
+      },
+      function(err, res, body) {
+        if (!err && res.statusCode == 200) {
+          if (body.success) {
+            // Get the members in the channel.
+            client.channels[channel] = body.members;
+            console.log("Successfully created channel " + channel + ", which has members " + body.members + ".");
+
+            resolve(body.members);
+          } else if (body.channel_exists) {
+            // If the channel exists already, we're set.
+            reject("Channel " + channel + " already exists.");
+          } else {
+            reject("An unhandled error occurred.");
+          }
+        } else {
+          resolve(exports.createChannel(channel));
+        }
       }
-    }
-  );
+    )
+  });
+
+  return promise;
 }
 
 // Requests the directory to add this user to a channel, and updates the membership list for that channel upon success.
 exports.joinChannel = function(channel) {
-  console.log("Joining channel " + channel + ".");
-  request.post(
-    client.directory + "/joinChannel",
-    {
-      json: {
-        user: client.username,
-        channel: channel,
-        address: client.address
-      }
-    },
-    function(err, res, body) {
-      if (!err && res.statusCode == 200) {
-        if (body.success) {
-          // Get the members in the channel.
-          client.channels[channel] = body.members;
-          console.log("Successfully joined channel " + channel + ", which has members " + body.members + ".");
-        } else if (!body.channel_exists) {
-          // If the channel didn't exist, we create it.
-          exports.createChannel(channel);
-        } else {
-          console.log("An unhandled error occurred.")
+  console.log("Trying to join channel " + channel + ".");
+  var promise = new Promise(function(resolve, reject) {
+    request.post(
+      client.directory + "/joinChannel",
+      {
+        json: {
+          user: client.username,
+          channel: channel,
+          address: client.address
         }
-      } else {
-        // If something went wrong (for example, a timeout), we try again.
-        exports.joinChannel(channel);
+      },
+      function(err, res, body) {
+        if (!err && res.statusCode == 200) {
+          if (body.success) {
+            // Get the members in the channel.
+            client.channels[channel] = body.members;
+            console.log("Successfully joined channel " + channel + ", which has members " + body.members + ".");
+
+            resolve(body.members);
+          } else if (!body.channel_exists) {
+            // If the channel didn't exist, we create it.
+            resolve(exports.createChannel(channel));
+          } else {
+            reject("An unhandled error occurred.");
+          }
+        } else {
+          resolve(exports.joinChannel(channel));
+        }
       }
-    }
-  );
+    );
+  });
+
+  return promise;
 }
 
 // Requests the directory to remove this user from a channel, and clears the membership list for that channel upon success.
 exports.leaveChannel = function(channel) {
-  console.log("Leaving channel " + channel + ".");
-  request.post(
-    client.directory + "/leaveChannel",
-    {
-      json: {
-        user: client.username,
-        channel: channel,
-        address: client.address
-      }
-    },
-    function(err, res, body) {
-      if (!err && res.statusCode == 200) {
-        if (body.success) {
-          client.channels[channel] = {};
-          console.log("Successfully left channel " + channel + ".");
-        } else if (body.channel_exists && !body.user_in_channel) {
-          console.log("User was not a member of channel " + channel + ".");
-        } else if (!body.channel_exists) {
-          console.log("Channel " + channel + " does not exist.");
-        } else {
-          console.log("An unhandled error occurred.")
+  console.log("Trying to leave channel " + channel + ".");
+  var promise = new Promise(function(resolve, reject) {
+    request.post(
+      client.directory + "/leaveChannel",
+      {
+        json: {
+          user: client.username,
+          channel: channel,
+          address: client.address
+        }
+      },
+      function(err, res, body) {
+        if (!err && res.statusCode == 200) {
+          if (body.success) {
+            delete client.channels[channel];
+            resolve("Successfully left channel " + channel + ".");
+          } else if (body.channel_exists && !body.user_in_channel) {
+            reject("User was not a member of channel " + channel + ".");
+          } else if (!body.channel_exists) {
+            reject("Channel " + channel + " does not exist.");
+          } else {
+            reject("An unhandled error occurred.")
+          }
         }
       }
-    }
-  );
+    );
+  });
+
+  return promise;
 }
--- client/client.js
+++ client/client.js
@@ -14,10 +14,8 @@ exports.log = new Tree();
 
 // Connect to a chat channel
 console.log("Connecting to channel1...");
-channels.joinChannel("channel1");
 
-// Wait 1 second before sending message to ensure that we've joined the channel.
-setTimeout(function() {
-  console.log("Sending message.")
-  messages.sendMessageToChannel("channel1", "Hello world!");
-}, 1000);
+channels.joinChannel("channel1").then(function() {
+  console.log("Sending message:");
+  return messages.sendMessageToChannel("channel1", "Hello world!"); // this doesn't return promise yet though
+});
