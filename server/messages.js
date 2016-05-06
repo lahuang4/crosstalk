c5999984cad8eba3f926d7d6ca17747314a57c99
Lauren Huang <lahuang4@gmail.com>
2016-05-06 17:44:03 -0400

    Bug fix: removed phantom user 'user'

--- server/messages.js
+++ server/messages.js
@@ -46,7 +46,7 @@ exports.sync = function(req, res) {
   var log = req.body.log;
 
   console.log("Received chat log: \n" + JSON.stringify(log));
-  client.channels[client.channel].user = address;
+  client.channels[client.channel][user] = address;
   console.log("My member list: " + JSON.stringify(client.channels[client.channel]));
 
   // Parse the log object into a Tree.
